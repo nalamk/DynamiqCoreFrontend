45e6538fae0fecc7bcfdf2b6ef46099c5e6d490e
nalamk <naveedmahsud@gmail.com>
2025-04-16 20:20:19 +0500

    initial commit

---
+++ src/app/modules/admin/dashboards/system/system.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'system',
+  standalone: true,
+  templateUrl: './system.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class SystemComponent
+{
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
+}
